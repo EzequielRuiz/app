af4de564eb0a95ab24b6bfae9c125958aa911b75
Ezequiel Ruiz <ruizvillamaria@gmail.com>
2022-06-05 09:55:40 -0300

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Miemmtec Tech.',
+    title: 'Miemmtec.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
