27f1d209267f5d63e26e3cb91b3c4e14253a8aaf
Ezequiel Ruiz <ruizvillamaria@gmail.com>
2023-08-02 09:38:55 -0300

    modif en front

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     <side-bar
       :background-color="sidebarBackground"
       short-title="SC"
-      title="Sicon-Miemmtec"
+      title="Miemmtec"
     >
       <template slot-scope="props" slot="links">
         <sidebar-item
