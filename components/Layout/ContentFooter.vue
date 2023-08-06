72d2e32165bb66fda75e4994a3a6e00d2f904c08
Ezequiel Ruiz <ruizvillamaria@gmail.com>
2023-08-06 11:21:05 -0300

    commit

---
+++ components/Layout/ContentFooter.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <ul class="nav">
+        <li class="nav-item">
+          <a
+            href="https://miemmtec.com.ar"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            miemmtec.com.ar
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="https://miemmtec.com.ar"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            Sobre Nosotros
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="https://miemmtec.com.ar"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            Blog
+          </a>
+        </li>
+      </ul>
+      <div class="copyright">
+        &copy; {{ year }}, made with <i class="tim-icons icon-heart-2"></i> by
+        
+        <a
+          href="https://miemmtec.com.ar"
+          target="_blank"
+          rel="noopener"
+          >MIEMMTEC</a
+        >
+        for a better web.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
